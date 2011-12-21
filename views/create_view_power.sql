08afe8560edf58f432f027f2b277be5346b3a642
Daniel Soto <danielrsoto@gmail.com>
2011-12-20 22:07:05 -0500

    creates view with power calculation

---
+++ views/create_view_power.sql
@@ -0,0 +1,32 @@
+-- creates view that calculates hourly averaged power
+-- currently does not properly account for midnight reset
+
+drop view view_power;
+
+create view view_power as
+select
+    *
+from
+    (select
+        meter_timestamp,
+        watthours,
+        meter_name,
+        ip_address,
+        watthours - lag(watthours,1)
+            over (partition by ip_address, meter_name
+            order by meter_timestamp)
+            as power,
+        meter_timestamp - lag(meter_timestamp, 1)
+            over (partition by ip_address, meter_name
+            order by meter_timestamp)
+            as time_difference
+    from
+         view_primary_log
+    order by
+        meter_timestamp)
+    as q
+-- how do i deal with midnight stuff?
+where
+    power > 0
+    and time_difference = '01:00:00'
+;
