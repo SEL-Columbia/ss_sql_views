3c8bea4a33a9b18373d3ac0d8109513bfd1c1ecd
Daniel Soto <danielrsoto@gmail.com>
2012-01-07 18:06:25 -0500

    psycopg2 -> sqlalchemy port of customer_energy.py

--- python/customer_energy.py
+++ python/customer_energy.py
@@ -1,38 +1,35 @@
-# 12/20/11 10:31 PM
-# fixme: needs to use view_primary_log
+import sqlalchemy as sa
 
-import psycopg2
-conn = psycopg2.connect('dbname=gateway')
-cursor = conn.cursor()
+# create metadata object
+metadata = sa.MetaData('postgres://postgres:postgres@localhost:5432/gateway')
 
+# define table object
+vpl = sa.Table('view_primary_log', metadata, autoload=True)
+
+# query parameters
 meter_name = 'ml03'
 date_start = '20111015'
 date_end = '20111201'
+ip_mains = '192.168.1.200'
 
-columns = ('sum(watthours)',
-           'meter_timestamp')
+# sum will show up with key 'sum_1'
+query = sa.select([sa.func.sum(vpl.c.watthours), vpl.c.meter_timestamp],
+                  whereclause=sa.and_(vpl.c.meter_name == meter_name,
+                                      vpl.c.meter_timestamp > date_start,
+                                      vpl.c.meter_timestamp < date_end,
+                                      vpl.c.ip_address != ip_mains),
+                  group_by=vpl.c.meter_timestamp,
+                  order_by=vpl.c.meter_timestamp)
+result = query.execute()
 
-query = """
-        select %s, %s
-        from view_primary_log
-        where meter_name = '%s' and
-              meter_timestamp > '%s' and
-              meter_timestamp < '%s' and
-              ip_address!='192.168.1.200'
-              group by meter_timestamp
-              order by meter_timestamp;
-        """ % (columns[0], columns[1], meter_name, date_start, date_end)
-
-cursor.execute(query)
-shniz = cursor.fetchall()
-
+# parse result into arrays
 dates = []
 watthours = []
+for r in result:
+    watthours.append(r.sum_1)
+    dates.append(r.meter_timestamp)
 
-for s in shniz:
-    dates.append(s[1])
-    watthours.append(s[0])
-
+# graph
 import pylab
 pylab.plot_date(dates,watthours,'ko-')
 pylab.grid()
