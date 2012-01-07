e0ed0c062de65622c491ac961a027d752d65e6da
Daniel Soto <danielrsoto@gmail.com>
2012-01-07 17:50:05 -0500

    updates to use view_primary_log

--- python/customer_energy.py
+++ python/customer_energy.py
@@ -10,17 +10,17 @@ date_start = '20111015'
 date_end = '20111201'
 
 columns = ('sum(watthours)',
-           'date')
+           'meter_timestamp')
 
 query = """
         select %s, %s
-        from primary_log_view
-        where name = '%s' and
-              date > '%s' and
-              date < '%s' and
+        from view_primary_log
+        where meter_name = '%s' and
+              meter_timestamp > '%s' and
+              meter_timestamp < '%s' and
               ip_address!='192.168.1.200'
-              group by date
-              order by date;
+              group by meter_timestamp
+              order by meter_timestamp;
         """ % (columns[0], columns[1], meter_name, date_start, date_end)
 
 cursor.execute(query)
