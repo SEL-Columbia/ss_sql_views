9c74011db2ebc8e044a8f61c09e9d7195005b111
Daniel Soto <danielrsoto@gmail.com>
2012-01-07 22:43:28 -0500

    changed histogram face colors

--- python/customer_histogram.py
+++ python/customer_histogram.py
@@ -9,7 +9,8 @@ vm = sa.Table('view_midnight', metadata, autoload=True)
 
 # query database
 query = sa.select([vm.c.watthours, vm.c.name],
-                   whereclause = sa.and_(vm.c.ip_address == '192.168.1.200',
+                   #whereclause = sa.and_(vm.c.ip_address == '192.168.1.200',
+                   whereclause = sa.and_(vm.c.ip_address != '192.168.1.200',
                                        vm.c.watthours>0))
 print query
 
@@ -29,12 +30,14 @@ import matplotlib.pyplot as plt
 
 f, ax = plt.subplots(1,1)
 import numpy as np
-ax.hist(watthour_list, bins=np.linspace(0,4000,41))
+#ax.hist(watthour_list, bins=np.linspace(0,4000,41), facecolor='#dddddd')
+ax.hist(watthour_list, bins=np.linspace(0,200,41), facecolor='#dddddd')
 #ax.hist(watthour_list)
 ax.set_xlabel('Daily Electrical Energy Consumed (Wh)')
 ax.set_ylabel('Number of Days Observed')
-#ax.set_title('Uganda Daily Electricity Consumption')
-ax.set_title('Mali Mains Electricity Consumption')
-f.savefig(country+'_mains_histogram.pdf')
+ax.set_title('Mali Daily Electricity Consumption')
+#ax.set_title('Uganda Mains Electricity Consumption')
+#f.savefig(country+'_mains_histogram.pdf')
+f.savefig(country+'_histogram.pdf')
 
 #plt.show()
