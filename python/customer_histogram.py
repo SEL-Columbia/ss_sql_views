d2d6920be9b0dfd6fc99c0f96577396cdf27c9d9
Daniel Soto <danielrsoto@gmail.com>
2012-01-07 22:24:39 -0500

    configured for mali mains plot

--- python/customer_histogram.py
+++ python/customer_histogram.py
@@ -9,7 +9,7 @@ vm = sa.Table('view_midnight', metadata, autoload=True)
 
 # query database
 query = sa.select([vm.c.watthours, vm.c.name],
-                   whereclause = sa.and_(vm.c.ip_address != '192.168.1.200',
+                   whereclause = sa.and_(vm.c.ip_address == '192.168.1.200',
                                        vm.c.watthours>0))
 print query
 
@@ -17,7 +17,7 @@ print query
 result = query.execute()
 
 country = 'ml'
-country = 'ug'
+#country = 'ug'
 
 watthour_list = []
 for r in result:
@@ -29,10 +29,12 @@ import matplotlib.pyplot as plt
 
 f, ax = plt.subplots(1,1)
 import numpy as np
-ax.hist(watthour_list, bins=np.linspace(0,200,41))
+ax.hist(watthour_list, bins=np.linspace(0,4000,41))
+#ax.hist(watthour_list)
 ax.set_xlabel('Daily Electrical Energy Consumed (Wh)')
 ax.set_ylabel('Number of Days Observed')
-ax.set_title('Uganda Daily Electricity Consumption')
-f.savefig(country+'_histogram.pdf')
+#ax.set_title('Uganda Daily Electricity Consumption')
+ax.set_title('Mali Mains Electricity Consumption')
+f.savefig(country+'_mains_histogram.pdf')
 
 #plt.show()
