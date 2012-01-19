1b3a44774563b41dd790d657d0f5a4fbdf190ac2
Daniel Soto <danielrsoto@gmail.com>
2012-01-19 16:42:39 -0500

    changing to use only midnight values

--- python/aggregated_customer_energy.py
+++ python/aggregated_customer_energy.py
@@ -15,10 +15,10 @@ Note: The meter_list variable must also be changed (around line 42)
 
 # query parameters
 import datetime as dt
-date_start = dt.datetime(2011,12,01)
-date_end = dt.datetime(2011,12,31)
+date_start = dt.datetime(2011,01,01)
+date_end = dt.datetime(2012,02,01)
 ip_mains = '192.168.1.200'
-figure_filename = 'uganda_dec.pdf'
+figure_filename = 'all_customers.pdf'
 
 def aggregated_customer_energy():
     import sqlalchemy as sa
@@ -27,7 +27,8 @@ def aggregated_customer_energy():
     metadata = sa.MetaData('postgres://postgres:postgres@localhost:5432/gateway')
 
     # define table objects from database
-    vpl = sa.Table('view_primary_log', metadata, autoload=True)
+    #vpl = sa.Table('view_primary_log', metadata, autoload=True)
+    vm = sa.Table('view_midnight', metadata, autoload=True)
     m = sa.Table('meter', metadata, autoload=True)
 
     # get meter list from database
@@ -39,24 +40,24 @@ def aggregated_customer_energy():
 
     # prune meter list for mali or uganda
     #meter_list = meter_list[:9]
-    meter_list = meter_list[9:]
+    #meter_list = meter_list[9:]
     print 'meters being reported are ', meter_list
 
     # graph
     import pylab
     import matplotlib.pyplot as plt
     import math
-    f, ax = plt.subplots(len(meter_list), 1)
+    f, ax = plt.subplots(len(meter_list), 1, sharex=True, sharey=True)
     f.set_size_inches((16, 16))
     for i, meter_name in enumerate(meter_list):
         # sum will show up with key 'sum_1'
-        query = sa.select([sa.func.sum(vpl.c.watthours), vpl.c.meter_timestamp],
-                          whereclause=sa.and_(vpl.c.meter_name == meter_name,
-                                              vpl.c.meter_timestamp > date_start,
-                                              vpl.c.meter_timestamp < date_end,
-                                              vpl.c.ip_address != ip_mains),
-                          group_by=vpl.c.meter_timestamp,
-                          order_by=vpl.c.meter_timestamp)
+        query = sa.select([sa.func.sum(vm.c.watthours), vm.c.meter_timestamp],
+                          whereclause=sa.and_(vm.c.meter_name == meter_name,
+                                              vm.c.meter_timestamp > date_start,
+                                              vm.c.meter_timestamp < date_end,
+                                              vm.c.ip_address != ip_mains),
+                          group_by=vm.c.meter_timestamp,
+                          order_by=vm.c.meter_timestamp)
         result = query.execute()
 
         # parse result into arrays
@@ -68,7 +69,8 @@ def aggregated_customer_energy():
 
         ax[i].plot_date(dates,watthours,label=meter_name)
         ax[i].set_xlim((date_start, date_end))
-        ax[i].legend()
+        ax[i].set_yticks((0,500,1000,1500))
+        ax[i].legend(loc=[1,0])
 
     #plt.show()
     f.suptitle('Aggregated Customer Watthours')
