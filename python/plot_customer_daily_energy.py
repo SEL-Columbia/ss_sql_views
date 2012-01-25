3483c7d895abf65fc83d9fafb9d581351c414b14
Daniel Soto <danielrsoto@gmail.com>
2012-01-24 19:55:57 -0500

    adds credit trace to circuit graphs

--- python/plot_customer_daily_energy.py
+++ python/plot_customer_daily_energy.py
@@ -24,7 +24,9 @@ def plot_customer_daily_energy():
     query = sa.select([vm.c.circuit_id,
                        vm.c.meter_name,
                        vm.c.ip_address],
-                       order_by=(vm.c.meter_name, vm.c.ip_address))
+                       order_by=(vm.c.meter_name, vm.c.ip_address)#,
+                       #limit=10
+                       )
     result = query.execute()
     circuit_list = []
     for r in result:
@@ -35,23 +37,31 @@ def plot_customer_daily_energy():
         print c
         filename = 'pcde-' + c[1] + '-' + c[2][-3:] + '.pdf'
         query = sa.select([vmid.c.watthours,
+                           vmid.c.credit,
                            vmid.c.meter_timestamp],
                            whereclause=sa.and_(vmid.c.circuit_id==c[0],
                                                vmid.c.meter_timestamp<date_end,
-                                               vmid.c.meter_timestamp>date_start),
+                                               vmid.c.meter_timestamp>date_start#,
+                                               #vmid.c.watthours>0
+                                               ),
                            order_by=vmid.c.meter_timestamp)
         result = query.execute()
         dates = []
         watthours = []
+        credit = []
         for r in result:
             dates.append(r.meter_timestamp)
             watthours.append(r.watthours)
+            credit.append(r.credit)
         # plot each circuit daily energy values for all time
-        f, ax = plt.subplots(1,1)
-        ax.plot_date(dates, watthours)
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Daily Watthours')
-        ax.set_title(filename)
+        f, ax = plt.subplots(2,1, sharex=True)
+        ax[0].plot_date(dates, watthours, mfc='#dddddd')
+        ax[0].set_xlabel('Date')
+        ax[0].set_ylabel('Daily Watthours')
+        ax[0].set_xlim((date_start, date_end))
+        ax[0].set_title(filename)
+        ax[1].plot_date(dates, credit, mfc='#dddddd', linestyle='-', color='k')
+        ax[1].set_ylabel('Credit at Midnight')
         f.autofmt_xdate()
         f.savefig(filename)
         plt.close()
