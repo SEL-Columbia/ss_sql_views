370e65d46b15298c33c01da98eae00c8f17059db
Daniel Soto <danielrsoto@gmail.com>
2012-01-24 19:56:22 -0500

    big whitespace indentation change

--- views/create_view_watthour_drops.sql
+++ views/create_view_watthour_drops.sql
@@ -4,23 +4,29 @@ drop view view_watthour_drops;
 
 create view view_watthour_drops as
 select * from (
-select
-    meter_timestamp,
-    watthours,
-    meter_name,
-    ip_address,
-    watthours - lag(watthours,1)
-        over (partition by ip_address, meter_name
-        order by meter_timestamp)
-        as power,
-    meter_timestamp - lag(meter_timestamp, 1)
-        over (partition by ip_address, meter_name
-        order by meter_timestamp)
-        as time_difference
-from
-     view_primary_log
-order by
-    meter_timestamp) as q
+    select
+        meter_timestamp,
+        watthours,
+        meter_name,
+        ip_address,
+        watthours - lag(watthours,1)
+            over (partition by ip_address, meter_name
+            order by meter_timestamp)
+            as power,
+        meter_timestamp - lag(meter_timestamp, 1)
+            over (partition by ip_address, meter_name
+            order by meter_timestamp)
+            as time_difference
+    from
+         view_primary_log
+    where
+        meter_timestamp > '2011-11-01'
+        and meter_timestamp < '2011-12-01'
+    order by
+        meter_timestamp
+
+) as q
 where
     power<0
+    and extract(hour from meter_timestamp)=1
 ;
