3544880e5ff434d7736e13941f3467fa3da9c686
Daniel Soto <danielrsoto@gmail.com>
2012-01-25 11:42:00 -0500

    changing heuristic on spurious_reset.py

--- python/spurious_reset.py
+++ python/spurious_reset.py
@@ -1,6 +1,16 @@
 '''
-loop over circuits, query number of midnight vs 1am resets
-plot meter, circuit_id, percentage of bad resets
+loop over circuits and count resets occurring at 11pm instead of midnight.
+
+use the view_power view as the base table.
+
+if change from 11pm to midnight is zero or positive, count as correct
+
+if change from 11pm to midnight is negative, count as incorrect
+
+output meter_name, incorrect counts, correct counts
+
+note: could speed up by close to a factor of two by doing a single query and then
+using numpy to count positive/non-zero and negative values in array.
 '''
 
 import sqlalchemy as sa
@@ -24,8 +34,8 @@ def get_list_of_circuits():
     return circuit_list
 
 
-date_start = dt.datetime(2011, 1, 1)
-date_end = dt.datetime(2012, 1, 1)
+date_start = dt.datetime(2011, 12, 1)
+date_end   = dt.datetime(2012,  2, 1)
 
 metadata = sa.MetaData('postgres://postgres:postgres@localhost:5432/gateway')
 vm = sa.Table('view_meter', metadata, autoload=True )
@@ -43,6 +53,7 @@ for r in result:
 
 # get list of circuits
 
+result_list = []
 for meter_name in meter_list:
     #print c
     query = sa.select([sa.func.count(vp.c.power).label('reset_count')],
@@ -50,9 +61,10 @@ for meter_name in meter_list:
                                            vp.c.meter_timestamp<date_end,
                                            vp.c.meter_name==meter_name,
                                            vp.c.power<0,
-                                           sa.extract('hour', vp.c.meter_timestamp)==1)
+                                           sa.extract('hour', vp.c.meter_timestamp)==0)
                      )
     result = query.execute()
+
     for r in result:
         print meter_name, r.reset_count,
 
@@ -60,7 +72,7 @@ for meter_name in meter_list:
                        whereclause=sa.and_(vp.c.meter_timestamp>date_start,
                                            vp.c.meter_timestamp<date_end,
                                            vp.c.meter_name==meter_name,
-                                           vp.c.power<0,
+                                           vp.c.power>=0,
                                            sa.extract('hour', vp.c.meter_timestamp)==0)
                      )
     result2 = query2.execute()
