7861c904391f16d82507ed73dee8f6a5d844f6a3
Daniel Soto <danielrsoto@gmail.com>
2012-02-11 14:27:25 -0500

    adds columns to view

--- views/create_view_meter.sql
+++ views/create_view_meter.sql
@@ -6,7 +6,10 @@ select
     m.id as meter_id,
     m.name as meter_name,
     c.ip_address,
-    c.pin
+    c.pin,
+    c.energy_max,
+    c.power_max,
+    c.account_id
 from circuit as c
 join meter as m
 on c.meter=m.id;
