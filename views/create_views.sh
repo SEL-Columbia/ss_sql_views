740243a49824aa4ca5a09d48f42937a036ac0681
Daniel Soto <danielrsoto@gmail.com>
2012-02-21 18:16:39 -0500

    adds more views to script

--- views/create_views.sh
+++ views/create_views.sh
@@ -1,3 +1,6 @@
 psql -d gateway -f create_view_primary_log.sql 
 psql -d gateway -f create_view_midnight.sql 
 psql -d gateway -f create_view_meter.sql
+psql -d gateway -f create_view_alarms.sql
+psql -d gateway -f create_view_solar.sql
+psql -d gateway -f create_view_recharge.sql
