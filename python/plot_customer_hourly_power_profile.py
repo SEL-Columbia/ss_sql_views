ba689e90eb9573a457f216b1f57896dd9f2100cd
Daniel Soto <danielrsoto@gmail.com>
2012-02-22 09:36:09 -0500

    lots of changes and improvements to power profile
    + add units to ylabel
    + change x label to hour of day
    + change ticks to 0,4,8,...24
    + change y range to -1, 24
    + make report for hour i equal to hour_i+1 - hour_i
    + add plot annotations
    + port to use circuit dict instead of list

--- python/plot_customer_hourly_power_profile.py
+++ python/plot_customer_hourly_power_profile.py
@@ -1,70 +1,78 @@
 '''
 plot_customer_hourly_power_profile.py
 -----------------------------
-PCHP
+PCHPP
 
 get full list of energy on an hourly basis from view_primary_log
 
 use pandas to do a one hour time lag
+power reported is over the hour
 
 plot hourly power overlaid
 
-
-
+warning: occasionally breaks due to:
+ERROR: An unexpected error occurred while tokenizing input
+The following traceback may be corrupted or invalid
+The error message is: ('EOF in multi-line statement', (301, 0))
 '''
 
 import sqlalchemy as sa
 import matplotlib.pyplot as plt
 import datetime as dt
 
-date_start = dt.datetime(2012, 1, 1)
-date_end = dt.datetime(2012, 2, 20)
+date_start = dt.datetime(2012, 1,  1)
+date_end   = dt.datetime(2012, 2, 20)
 
 if __name__ == '__main__':
 
     import offline_gateway as og
-    circuit_list = og.get_circuit_list()
+    cdl = og.get_circuit_dict_list()
 
     debug = False
     #debug = True
     if debug:
-        circuit_list = circuit_list[:2]
-
-
+        cdl = cdl[:2]
 
     # iterate over list of circuits
-    for c in circuit_list:
-        filename = 'pchpp-' + c[1] + '-' + c[2][-3:] + '.pdf'
+    for c in cdl:
+
+        # query for watthours
+        filename = 'pchpp-' + c['meter_name'] + '-' + c['ip_address'][-3:] + '.pdf'
         print 'querying for', filename
-
         import offline_gateway as og
-        df = og.get_watthours_for_circuit_id(c[0], date_start, date_end)
+        df = og.get_watthours_for_circuit_id(c['circuit_id'], date_start, date_end)
 
-        # offset by 1 hour
+        # offset by 1 hour and subtract
         import pandas as p
-        offset = df - df.shift(1, offset=p.DateOffset(hours=1))
+        offset = df.shift(-1, offset=p.DateOffset(hours=1)) - df
 
+        # screen out negative values resulting from drops
         positive_only = True
         if positive_only:
             offset = offset[offset.values >= 0]
 
-
+        # extract hour information from series index for plotting
         hour = [ind.hour for ind in offset.index]
 
-        #1/0
-
-        # iterate over series and plot hour, value as pairs
-        # or create two lists from series
-
-        # plot each circuit daily energy values for all time
-        f, ax = plt.subplots(1, 1, sharex=True)
-
+        # plotting of hour vs. power values to create profile
+        f = plt.figure()
+        ax = f.add_axes((0.2,0.2,0.6,0.6))
         ax.plot(hour, offset.values, linestyle='',
                 marker='o', mec='#ffffff', alpha=0.2, mfc=None)
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Average Hourly Power')
-        #ax.set_xlim((date_start, date_end))
+        ax.set_xlabel('Hour of Day')
+        ax.set_ylabel('Average Hourly Power (W)')
+        ax.set_xticks((0,4,8,12,16,20,24))
+        ax.set_xlim((-1, 25))
         ax.set_title(filename)
-        f.autofmt_xdate()
-        f.savefig(filename)
+
+        # add annotations to plot
+        annotation = []
+        annotation.append('plot generated ' + str(dt.datetime.now()))
+        annotation.append('date start = ' + str(date_start))
+        annotation.append('date end = ' + str(date_end))
+        annotation = '\n'.join(annotation)
+        f.text(0.01,0.01, annotation)
+
+        # save to file
+        f.savefig(filename, transparent=True)
         plt.close()
