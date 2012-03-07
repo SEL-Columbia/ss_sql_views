5e10dc97e311c92521ed2550d0c1b410c7398de5
Daniel Soto <danielrsoto@gmail.com>
2012-03-07 10:58:27 -0500

    rewrite of histogram script

--- python/customer_energy_histogram.py
+++ python/customer_energy_histogram.py
@@ -4,49 +4,74 @@ customer_energy_histogram.py
 Creates a histogram of non-zero individual customer midnight samples over
 a given date range and given country.
 '''
-def customer_energy_histogram():
-    import sqlalchemy as sa
 
-    # create metadata object
-    metadata = sa.MetaData('postgres://postgres:postgres@localhost:5432/gateway')
+#if __name__ == '__main__':
+import offline_gateway as og
 
-    # define table objects from database
-    vm = sa.Table('view_midnight', metadata, autoload=True)
+# get date range
+import datetime as dt
+date_start = dt.datetime(2012, 1, 1)
+date_end = dt.datetime(2012, 2, 1)
 
-    # query database
-    query = sa.select([vm.c.watthours, vm.c.name],
-                       #whereclause = sa.and_(vm.c.ip_address == '192.168.1.200',
-                       whereclause = sa.and_(vm.c.ip_address != '192.168.1.200',
-                                           vm.c.watthours>0))
-    print query
+# select based on meter list?
+circuit_dict_list = og.get_circuit_dict_list(mains=False)
+meter_list = ['ml01', 'ml02', 'ml03', 'ml04', 'ml05', 'ml06', 'ml07', 'ml08']
+#meter_list = ['ug01', 'ug02', 'ug03', 'ug04', 'ug05', 'ug06', 'ug07', 'ug08']
+# get daily energy for each circuit
 
-    # stuff array
-    result = query.execute()
+import pandas as p
+import numpy as np
+all_energy = p.Series()
+all_energy = np.array([])
+num_circuits = 0
+for cd in circuit_dict_list:
+    #print 'querying'
+    if cd['meter_name'] not in meter_list:
+        continue
+    num_circuits += 1
+    print num_circuits
+    de = og.get_daily_energy_for_circuit_id(cd['circuit_id'], date_start, date_end)
+    if de == None:
+        continue
+    #de.values.astype(float)
+    all_energy = np.hstack((all_energy, de.values))
 
-    country = 'ml'
-    #country = 'ug'
+    # append de.values to all_energy
+#1/0
 
-    watthour_list = []
-    for r in result:
-        if country in r.name:
-            watthour_list.append(r.watthours)
+# remove zeros
+remove_zeros = True
+#remove_zeros = False
+if remove_zeros:
+    all_energy = all_energy[all_energy > 0]
+# concatenate
 
-    # graph histogram
-    import matplotlib.pyplot as plt
+# plot histogram
+import matplotlib.pyplot as plt
+f = plt.figure()
+ax = f.add_axes((0.2,0.2,0.6,0.6))
+import numpy as np
+#ax.hist(watthour_list, bins=np.linspace(0,4000,41), facecolor='#dddddd')
+ax.hist(all_energy, bins=np.linspace(0,200,41), facecolor='#dddddd', normed=True)
+#ax.hist(watthour_list)
+ax.set_xlabel('Daily Electrical Energy Consumed (Wh)')
+ax.set_ylabel('Frequency of Observation')
 
-    f, ax = plt.subplots(1,1)
-    import numpy as np
-    #ax.hist(watthour_list, bins=np.linspace(0,4000,41), facecolor='#dddddd')
-    ax.hist(watthour_list, bins=np.linspace(0,200,41), facecolor='#dddddd')
-    #ax.hist(watthour_list)
-    ax.set_xlabel('Daily Electrical Energy Consumed (Wh)')
-    ax.set_ylabel('Number of Days Observed')
-    ax.set_title('Mali Daily Electricity Consumption')
-    #ax.set_title('Uganda Mains Electricity Consumption')
-    #f.savefig(country+'_mains_histogram.pdf')
-    f.savefig(country+'_histogram.pdf')
+# add annotations to plot
+annotation = []
+annotation.append('plot generated ' + str(dt.datetime.now()))
+annotation.append('date start = ' + str(date_start))
+annotation.append('date end = ' + str(date_end))
+annotation.append('meter list = ' + str(meter_list))
+annotation = '\n'.join(annotation)
+f.text(0.01,0.01, annotation)
 
-    #plt.show()
+#ax.set_title('Mali Daily Electricity Consumption')
+#ax.set_title('Uganda Mains Electricity Consumption')
+#f.savefig(country+'_mains_histogram.pdf')
+f.savefig('mali_histogram.pdf')
+f.savefig('uganda_histogram.pdf')
 
-if __name__ == '__main__':
-    customer_energy_histogram()
+print 'number of datapoints = ', len(all_energy)
+print 'possible observations = ', num_circuits * (date_end - date_start).days
+
